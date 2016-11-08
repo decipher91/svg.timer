f73673fb06a3bc64b4e67018ed8dfef491bf32a0
Vitaly Maltsev <vitaly.maltsev@elinext.com>
2016-11-08 10:57:36 +0300

    seconds break to date

--- src/svgTimer.js
+++ src/svgTimer.js
@@ -1,9 +1,36 @@
 (function ( $ ) {
+    function setText(opts, counter, i){
+        if(opts.direction === 'forward'){
+            counter.text(i);
+        } else if (opts.direction === 'backwards') {
+            var count = opts.time - i;
+            counter.text(count);
+        } else {
+            counter.text(i);
+        }
+    }
+
+    var testDate = '2017-01-23T00:00:00';
+
+    function parseDate(date){
+        var countDownDate = new Date(date);
+        console.log(countDownDate);
+        var dateNow = new Date();
+        var diff = (countDownDate - dateNow)/1000;
+        var d = Math.floor(diff / 86400);
+        var h = Math.floor( diff % 86400 / 3600);
+        var m = Math.floor(diff % 86400 % 3600 / 60 );
+        var s = Math.floor(diff % 86400 % 3600 % 60 );
+        console.log(d + ' days, ' + h + ' hours, ' + m + ' minutes, ' + s + ' seconds');
+    }
+
+    parseDate(testDate);
+
     $.fn.svgTimer = function(options) {
         var opts = $.extend({}, $.fn.svgTimer.defaults, options);
 
         var template = "<div class='svg-hexagonal-counter'>"
-            + "<h2>0</h2>"
+            + "<div class='hint'><span class='hint-count'></span><br><span class='hint-text'></span></div>"
             + "<svg class='counter' x='0px' y='0px' viewBox='0 0 776 628'>"
             + "<path class='track' d='M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z'></path>"
             + "<path class='fill' d='M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z'></path>"
@@ -18,12 +45,18 @@
             //define dom elements
             var track = parentEl.find('.track');
             var fill = parentEl.find('.fill');
-            var counterText = parentEl.find('h2');
+            var hintCount = parentEl.find('.hint-count');
+            var hintText = parentEl.find('.hint-text').text(opts.hint);
+
 
             //set time and offset
             var time = opts.time; /* how long the timer runs for */
             var initialOffset = 2160;
             var i = 1;
+
+            //set counter text
+            setText(opts, hintCount, i);
+
 
             //draw initial hexagon
             track.css('stroke', opts.track);
@@ -35,18 +68,13 @@
 
             //run timer
             var interval = setInterval(function() {
-                track.css('stroke', opts.track);
+                //track.css('stroke', opts.track);
                 fill.css({
                     'stroke': opts.fill,
                     'stroke-dashoffset': initialOffset-(i*(initialOffset/time)) + 'px',
                     'transition': 'stroke-dashoffset 1s ' +  opts.transition
                 });
-                if(opts.direction === 'forward'){
-                    counterText.text(i);
-                } else if (opts.direction === 'backward') {
-                    var count = opts.time - i + 1;
-                    counterText.text(count);
-                }
+                setText(opts, hintCount, i);
 
                 if (i == time) {
                     clearInterval(interval);
@@ -58,10 +86,12 @@
 
     $.fn.svgTimer.defaults = {
         time: 60,
+        interval: 1000,
+        direction: 'forward',
         track: 'rgb(56, 71, 83)',
         fill: 'rgb(104, 214, 198)',
         transition: 'linear',
-        direction: 'forward',
-        interval: 1000
+        hint: 'seconds'
+
     }
 }( jQuery ));
---
+++ src/svgcountdown.js
@@ -0,0 +1,3 @@
+/**
+ * Created by decipher on 8.11.16.
+ */
