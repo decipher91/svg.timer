523cd659070cd4802df520b16c8c9f2fa3fe312b
Vitaly Maltsev <vitaly.maltsev@elinext.com>
2016-11-11 10:03:18 +0300

    countdown complete

--- src/svgcountdown.js
+++ src/svgcountdown.js
@@ -2,37 +2,50 @@
  * Created by decipher on 8.11.16.
  */
 (function ($) {
+    'use strict';
     function setText(time, counter) {
-        var count = 60 - time;
+        var count;
+        if (time === 0) {
+            count = 0;
+        } else {
+            count = 60 - time
+        }
 
         counter.text(count);
     }
 
     function setInitialText(time, counter){
-        console.log('setting ' + counter);
         counter.text(time);
     }
 
     var testDate = '2017-01-23T00:00:00';
 
     function parseDate(date) {
-        var countDownDate = new Date(date);
-        var dateNow = new Date();
-        var diff = (countDownDate - dateNow) / 1000;
-        var d = Math.floor(diff / 86400);
-        var h = Math.floor(diff % 86400 / 3600);
-        var m = Math.floor(diff % 86400 % 3600 / 60);
-        var s = Math.floor(diff % 86400 % 3600 % 60);
-        console.log(d + ' days, ' + h + ' hours, ' + m + ' minutes, ' + s + ' seconds');
+        var dfd = jQuery.Deferred();
+        var dateToParse = date;
 
-        return {
-            date: countDownDate,
-            diff: diff,
-            d: d,
-            h: h,
-            m: m,
-            s: s
-        }
+        setTimeout(function() {
+            var countDownDate = new Date(dateToParse);
+            var dateNow = new Date();
+            var diff = (countDownDate - dateNow) / 1000;
+            var d = Math.floor(diff / 86400);
+            var h = Math.floor(diff % 86400 / 3600);
+            var m = Math.floor(diff % 86400 % 3600 / 60);
+            var s = Math.floor(diff % 86400 % 3600 % 60);
+            console.log(d + ' days, ' + h + ' hours, ' + m + ' minutes, ' + s + ' seconds');
+            var date = {
+                date: countDownDate,
+                diff: diff,
+                d: d,
+                h: h,
+                m: m,
+                s: s
+            };
+            dfd.resolve(date);
+        });
+
+        return dfd.promise();
+
     }
 
     $.fn.svgCountDown = function (options) {
@@ -70,120 +83,128 @@
             + "</svg>"
             + "</div>";
 
-        var date = parseDate(opts.date);
-        console.log(date);
+        var date;
+        var $this = this;
 
-        return this.each(function () {
-            // Build dom for svg countdown
-            var parentEl = $(this);
-            parentEl.append(template);
-
-            //define dom elements
-            var track = parentEl.find('.track');
-            var daysCountDown = parentEl.find('.svg-days-counter');
-            var daysTrack = daysCountDown.find('.track');
-            var daysFill = daysCountDown.find('.fill');
-            var daysHintCount = daysCountDown.find('.hint-count');
-
-            var hoursCountDown = parentEl.find('.svg-hours-counter');
-            //var hoursTrack = hoursCountDown.find('.track');
-            var hoursFill = hoursCountDown.find('.fill');
-            var hoursHintCount = hoursCountDown.find('.hint-count');
-
-            var minutesCountDown = parentEl.find('.svg-minutes-counter');
-            //var minutesTrack = minutesCountDown.find('.track');
-            var minutesFill = minutesCountDown.find('.fill');
-            var minutesHintCount = minutesCountDown.find('.hint-count');
-
-            var secsCountDown = parentEl.find('.svg-seconds-counter');
-            //var secsTrack = secsCountDown.find('.track');
-            var secsFill = secsCountDown.find('.fill');
-            var secsHintCount = secsCountDown.find('.hint-count');
-
-            /* how long the timer runs for */
-            var initialOffset = 2160;
-            var i = date.s;
-
-            setInitialText(date.d, daysHintCount);
-            setInitialText(date.h, hoursHintCount);
-            setInitialText(date.m, minutesHintCount);
-            setInitialText(date.s, secsHintCount);
-
-            //draw initial hexagon
-            track.css('stroke', opts.track);
-
-            function setCounter(){
-                daysFill.css({
-                    'stroke': opts.fill,
-                    'stroke-dashoffset': initialOffset - (initialOffset / date.d)
-                    //'transition': 'stroke-dashoffset 1s ' + opts.transition
-                });
+        parseDate(opts.date).then(function(status, data){
+            date = status;
+            runCountDown();
+        });
 
 
-                hoursFill.css({
-                    'stroke': opts.fill,
-                    'stroke-dashoffset': initialOffset - ((initialOffset / 24) * date.h )
-                    //'transition': 'stroke-dashoffset 1s ' + opts.transition
-                });
 
-                minutesFill.css({
-                    'stroke': opts.fill,
-                    'stroke-dashoffset': initialOffset - ((initialOffset / 60) * date.m)
-                    //'transition': 'stroke-dashoffset 1s ' + opts.transition
-                });
-            }
+        function runCountDown(){
+            return $this.each(function () {
+                // Build dom for svg countdown
+                var parentEl = $(this);
+                parentEl.append(template);
+
+                //define dom elements
+                var track = parentEl.find('.track');
+                var daysCountDown = parentEl.find('.svg-days-counter');
+                var daysFill = daysCountDown.find('.fill');
+                var daysHintCount = daysCountDown.find('.hint-count');
+
+                var hoursCountDown = parentEl.find('.svg-hours-counter');
+                var hoursFill = hoursCountDown.find('.fill');
+                var hoursHintCount = hoursCountDown.find('.hint-count');
+
+                var minutesCountDown = parentEl.find('.svg-minutes-counter');
+                var minutesFill = minutesCountDown.find('.fill');
+                var minutesHintCount = minutesCountDown.find('.hint-count');
+
+                var secsCountDown = parentEl.find('.svg-seconds-counter');
+                var secsFill = secsCountDown.find('.fill');
+                var secsHintCount = secsCountDown.find('.hint-count');
+
+                /* how long the timer runs for */
+                var initialOffset = 2160;
+                var i = date.s;
+
+                setInitialText(date.d, daysHintCount);
+                setInitialText(date.h, hoursHintCount);
+                setInitialText(date.m, minutesHintCount);
+                setText(i, secsHintCount);
+
+                //draw initial hexagon
+                track.css('stroke', opts.track);
+
+                function setCounter(){
+                    daysFill.css({
+                        'stroke': opts.fill,
+                        'stroke-dashoffset': initialOffset - (initialOffset / date.d),
+                        'transition': 'stroke-dashoffset 1s ' + opts.transition
+                    });
 
 
-            setCounter();
-            secsFill.css({
-                'stroke': opts.fill,
-                'stroke-dashoffset': initialOffset - ((initialOffset / 60) * date.s)
-            });
+                    hoursFill.css({
+                        'stroke': opts.fill,
+                        'stroke-dashoffset': initialOffset - ((initialOffset / 24) * (24 - date.h) ),
+                        'transition': 'stroke-dashoffset 1s ' + opts.transition
+                    });
 
-            //run timer
-            var interval = setInterval(function () {
+                    minutesFill.css({
+                        'stroke': opts.fill,
+                        'stroke-dashoffset': initialOffset - ((initialOffset / 60) * (60 - date.m) ),
+                        'transition': 'stroke-dashoffset 1s ' + opts.transition
+                    });
+                }
+
+
+                setCounter();
                 secsFill.css({
                     'stroke': opts.fill,
-                    'stroke-dashoffset': initialOffset - ((initialOffset / 60)* i),
-                    'transition': 'stroke-dashoffset 1s ' + opts.transition
-
+                    'stroke-dashoffset': initialOffset - ((initialOffset / 60) * date.s)
                 });
-                //console.log(i * (initialOffset /  date.s));
-                //console.log(initialOffset / 60);
-                //if(date.s )
-                setText(i, secsHintCount);
-                //console.log(i);
-                //console.log(date.s);
-                if (i == 60) {
-                    console.log(i);
-                    console.log(date.s);
-                    console.log('time to rewrite');
-                    //date.s = 60;
-                    //i++;
-                    parseDate(opts.date);
-                    setCounter();
-                    console.log(date.m);
-                    setInitialText(date.d, daysHintCount);
-                    setInitialText(date.h, hoursHintCount);
-                    setInitialText(date.m, minutesHintCount);
+
+                //run timer
+                var interval = setInterval(function () {
+
+                    if (i == 60) {
+                        i = 0;
+                        parseDate(opts.date).then(function(status){
+                            date = status;
+                            console.log(status);
+                            setCounter();
+                            setInitialText(date.d, daysHintCount);
+                            setInitialText(date.h, hoursHintCount);
+                            setInitialText(date.m, minutesHintCount);
+                            //setText(i, secsHintCount);
+
+                        });
+                    }
                     setText(i, secsHintCount);
-                    i = 0;
-                    /*secsFill.css({
-                        //'stroke-dashoffset': initialOffset - (i * (initialOffset / date.s)),
-                        'stroke-dashoffset': initialOffset - ((i * (initialOffset / 60)) * date.s)
-                    });*/
-                    //clearInterval(interval);
-                }
-                i++;
-            }, 1000);
-        });
+                    if (i == 0){
+                        secsFill.css({
+
+                            'stroke': opts.fill,
+                            'stroke-dashoffset': initialOffset - ((initialOffset / 60)* i),
+                            'transition': 'none '
+                        });
+                    } else {
+                        secsFill.css({
+                            'stroke': opts.fill,
+                            'stroke-dashoffset': initialOffset - ((initialOffset / 60)* i),
+                            'transition': 'stroke-dashoffset 1s ' + opts.transition
+
+                        });
+
+                    }
+                    i++;
+                }, 1000);
+            });
+        }
+
+
     };
 
     $.fn.svgCountDown.defaults = {
         date: '2017-01-23T00:00:00',
         direction: 'forward',
-        track: 'rgb(56, 71, 83)',
-        fill: 'rgb(104, 214, 198)',
+        //track: 'rgb(56, 71, 83)',
+        track: '#582C27',
+        //fill: 'rgb(104, 214, 198)',
+        fill: '#DD1F2C',
         transition: 'linear'
     }
 }(jQuery));
